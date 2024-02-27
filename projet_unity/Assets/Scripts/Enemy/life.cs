4003d6bce42022457f51351f3559cdbd550e1498
GurvanN22 <gurvan00722@outlook.com>
2024-02-27 08:45:40 +0100

    add spauwn

--- projet_unity/Assets/Scripts/Enemy/life.cs
+++ projet_unity/Assets/Scripts/Enemy/life.cs
@@ -2,36 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class life : MonoBehaviour
+namespace MyNamespace 
 {
-    // Start is called before the first frame update
-
-    public int life_points = 1;
-    void Start()
+    public class life : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-    }
-
-
-    public void take_damage(int damage)
-    {
-        life_points -= damage;
-        if (life_points <= 0)
+        public int life_points = 2;
+        void Start()
         {
-            Destroy(gameObject);
+
         }
-    }
-    
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if(other.CompareTag("Bullet")) 
+        void Update()
         {
-            take_damage(1);
+            Rushplayer();
         }
+
+
+        public void take_damage(int damage)
+        {
+            life_points -= damage;
+            if (life_points <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Bullet"))
+            {
+                take_damage(1);
+            }
+        }
+
+
+
+        public float speed = 5f; 
+
+
+        void Rushplayer()
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+            if (player != null)
+            {
+                Vector3 direction = player.transform.position - transform.position;
+                transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+                float distance = Vector3.Distance(transform.position, player.transform.position);
+                if (distance < 1f) 
+                {
+                    PlayerDeath();
+                }
+            }
+        }
+
+        void PlayerDeath() 
+        {
+            Debug.Log("Le joueur est mort !");
+        }
+
     }
 }
