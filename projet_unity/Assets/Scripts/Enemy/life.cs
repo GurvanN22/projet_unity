caf8564035fec1d0abbab783af2884e4268a0e57
doska <nxa.drxuineau@ynov.com>
2024-02-29 18:00:41 +0100

    add navmesh

--- projet_unity/Assets/Scripts/Enemy/life.cs
+++ projet_unity/Assets/Scripts/Enemy/life.cs
@@ -1,22 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-namespace MyNamespace 
+namespace MyNamespace
 {
     public class life : MonoBehaviour
     {
+        [SerializeField] Transform target;
+        NavMeshAgent agent;
 
         public int life_points = 2;
+
         void Start()
         {
-
+            agent = GetComponent<NavMeshAgent>();
+            agent.updateRotation = false;
+            agent.updateUpAxis = false;
+            target = GameObject.FindGameObjectsWithTag("Player")[0].transform;
         }
+
         void Update()
         {
-            Rushplayer();
+            agent.SetDestination(target.position);
+            // Rushplayer();
         }
-
 
         public void take_damage(int damage)
         {
@@ -34,36 +42,5 @@ namespace MyNamespace
                 take_damage(1);
             }
         }
-
-
-
-        public float speed = 5f; // Vitesse de déplacement de l'ennemi
-
-
-        void Rushplayer()
-        {
-            GameObject player = GameObject.FindGameObjectWithTag("Player");
-
-            if (player != null)
-            {
-                // Déplacement vers le joueur
-                Vector3 direction = player.transform.position - transform.position;
-                transform.Translate(direction.normalized * speed * Time.deltaTime);
-
-                // Vérification de la collision avec le joueur
-                float distance = Vector3.Distance(transform.position, player.transform.position);
-                if (distance < 1f) // Changer cette valeur selon votre besoin
-                {
-                    // Collision avec le joueur, gérer la mort du joueur ici
-                    PlayerDeath();
-                }
-            }
-        }
-
-        void PlayerDeath() 
-        {
-            Debug.Log("Le joueur est mort !");
-        }
-
     }
 }
