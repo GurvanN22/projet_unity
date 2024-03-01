487e74e6bfacd2fe3c2c7384b851241dfb6a69f4
aenzo <enzo.achard@ynov.com>
2024-03-01 18:07:52 +0100

    main

--- projet_unity/Assets/Scripts/MAINMENU.cs
+++ projet_unity/Assets/Scripts/MAINMENU.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MAINMENU : MonoBehaviour
 {
     public string levelToLoad;
+    public GameObject settingsWindow;
     
     public void StartGame()
     {
